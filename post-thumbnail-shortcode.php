efd067b41e5e8adc564592647b79c99825da56cc
dandyxu <dandyjefferson@gmail.com>
2017-05-11 14:57:44 +0100

    Basic Function realized, but only display 'post_content'

--- post-thumbnail-shortcode.php
+++ post-thumbnail-shortcode.php
@@ -8,3 +8,40 @@
     Author URI: https://github.com/dandyxu
  */
 
+function post_thumbnail_shortcode($content = null){
+    // Check if there is existing function called "post_thumbnail_shortcode"
+    if(!function_exists('post_thumbnail_shortcode'))
+        return;
+
+    // The Query
+        $the_query = new WP_Query( array( 'post_type' => 'what_student_matter') );
+
+    // The Loop
+        if ( $the_query->have_posts() ) {
+
+            // Get Recent Post
+            $args = array( 'numberposts' => '1', 'post_type' => 'what_student_matter' );
+            $recent_posts = wp_get_recent_posts( $args );
+
+            //echo '<ul>';
+            foreach( $recent_posts as $recent ){
+                //echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"].'</a> </li> ';
+                $content = $recent["post_content"];
+            }
+
+            //while ( $the_query->have_posts() ) {
+            //    $the_query->the_post();
+            //    echo '<li>' . get_the_title() . '</li>';
+            //}
+            //echo '</ul>';
+            /* Restore original Post Data */
+            wp_reset_postdata();
+
+        } else {
+            // no posts found
+        }
+
+        return $content;
+}
+
+add_shortcode('post-thumbnail', 'post_thumbnail_shortcode');
