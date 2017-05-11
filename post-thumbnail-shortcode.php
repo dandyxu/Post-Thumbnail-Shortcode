b6f5260a40764917e2054ad2274e4a66745a8e6c
dandyxu <dandyjefferson@gmail.com>
2017-05-11 15:01:27 +0100

    Fix Comments in the plugin header

--- post-thumbnail-shortcode.php
+++ post-thumbnail-shortcode.php
@@ -1,11 +1,11 @@
 <?php
 /**
-    Plugin Name: Post Thumbnail Shortcode
-    Plugin URI: http://localhost/CyerBeta170404/
-    Description: This plugin will provide a shortcode to display the thumbnail of the most recent post in What Matters for Students category
-    Author: Dandy Xu
-    Version: 2.1
-    Author URI: https://github.com/dandyxu
+  *Plugin Name: Post Thumbnail Shortcode
+  *Plugin URI: http://localhost/CyerBeta170404/
+  *Description: This plugin will provide a shortcode to display the thumbnail of the most recent post in What Matters for Students category
+  *Author: Dandy Xu
+  *Version: 2.1
+  *Author URI: https://github.com/dandyxu
  */
 
 function post_thumbnail_shortcode($content = null){
