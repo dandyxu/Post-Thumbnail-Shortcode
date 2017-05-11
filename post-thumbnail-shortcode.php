86eb9cab1eee5b36a9e92f3fe1b98bfcecb31d85
dandyxu <dandyjefferson@gmail.com>
2017-05-11 12:04:09 +0100

    Initial commit

---
+++ post-thumbnail-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+/**
+    Plugin Name: Post Thumbnail Shortcode
+    Plugin URI: http://localhost/CyerBeta170404/
+    Description: This plugin will provide a shortcode to display the thumbnail of the most recent post in What Matters for Students category
+    Author: Dandy Xu
+    Version: 2.1
+    Author URI: https://github.com/dandyxu
+ */
+
